091cc26fca109d55c3a21a2913046914ebba61b8
aprakash97 <prakashakrakr@gmail.com>
2020-09-08 08:05:01 +0530

    r1

---
+++ app/src/main/java/com/example/personalplanner/Alarm1.java
@@ -0,0 +1,14 @@
+package com.example.personalplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Alarm1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm1);
+    }
+}
---
+++ app/src/main/java/com/example/personalplanner/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.personalplanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.Time);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openAlarm1();
+            }
+        });
+        }
+
+    private void openAlarm1() {
+        Intent intent = new Intent(this, Alarm1.class);
+        startActivity(intent);
+    }
+}
